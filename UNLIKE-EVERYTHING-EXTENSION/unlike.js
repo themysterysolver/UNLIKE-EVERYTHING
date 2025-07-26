40e01dd445ea3511111bf975c2750d032290802f
themysterysolver <arjunprabhakar1910@gmail.com>
2025-07-26 20:12:45 +0530

    CHORE:commenting

--- UNLIKE-EVERYTHING-EXTENSION/unlike.js
+++ UNLIKE-EVERYTHING-EXTENSION/unlike.js
@@ -20,20 +20,20 @@ async function deleteLikedVideos(){
         document.querySelectorAll('*').forEach((el)=>{
             if(el.innerHTML.trim() =='Select'){a = el}
         });
-        console.log("C1");
+        //console.log("C1");
         if(!a){
             console.log("No more liked videos!");
             break;
         }
         a.click();
         await waitForIt(1000);
-        console.log("C2");
+        //console.log("C2");
         const images = document.querySelectorAll('[data-bloks-name="bk.components.Image"]');
         for(const img of images){
             img.click();
             await waitForIt(500);    
         }
-        console.log("C3");
+        //console.log("C3");
         let b = null;
         document.querySelectorAll('[data-bloks-name="bk.components.TextSpan"]').forEach((el)=>{
             if(el.innerHTML.trim().includes('Unlike')){b = el}
@@ -42,7 +42,7 @@ async function deleteLikedVideos(){
             b.click();
             await waitForIt(1000)
         }
-        console.log("C4");
+        //console.log("C4");
         const delBtn = document.querySelector('button[tabindex="0"]');
         if(delBtn){
             delBtn.click();
